4a21f6f1f6ef764f5da21e8ed84cc425bae86545
Stanley Surjanto <stan.surjanto@gmail.com>
2022-08-31 23:55:18 -0600

    created new module for just the fs functions, and imported them into fetcher.js to use in the request callback. Logic of overwriting is still broken though...

--- fetcher.js
+++ fetcher.js
@@ -2,70 +2,39 @@
 const args = process.argv.slice(2);
 console.log(args);
 console.log(`--------------------------------------------------------------------------------`)
-
-// Imports filesystem api 
-const fs = require('fs');
 // Imports our request package
 const request = require('request');
-// Imports readline for user prompts
-const readline = require('readline');
-const { stdin, stdout } = require('process');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const {rl, writeTo, readFrom } = require('./fsFuncs');
 
 
-// Testing out fs.writeFile => writes our file to something called index.html, creates it if not present
-// const content = "hello"
-// fs.writeFile('./index.html', content, error => {
-//   if (error) {
-//     console.log(error);
-//   } 
-// });
 
 // Requests HTTP from args[0]
 request(args[0], (error, response, body) => {
+  const localPath = args[1];
   console.log(`error: ${error}`); // logs error if there is one
   console.log(`statuscode: ${response && response.statusCode}`) // gives our status code
 
-  
-  
-  // EC 1: if file path already exists => let user know, command prompt "Y -> enter" to overwrite
+  // EC 1: if file path already exists => let user know, command prompt "Y -> enter" to overwrite 
   // otherwise skip & process.exit();
-  if (args[1]) {
+
+  // err... still reading as if there is already a file when there is none...
+  if (localPath) {
     rl.question("The file path already exists.\nDo you want to overwrite it? (Y/N)", (ans) => {
       // case insensitive
       if(ans.toLowerCase() === "y") {
         console.log(`${ans} - Alright, overwriting...`);
-        // Then, writes the body into the filename of args[1], and overwrites
-        fs.writeFile(args[1], body, error => {
-          if(error) { console.log( error )}; // logs error if there is one
-        });
+        // Then, writes the body into the filename of localPath, and overwrites
+        writeTo(localPath, body);
         // Reads the file when done to return total bytes, 1char = 1byte
-        fs.readFile(args[1], (error, data) => {
-          if(error) { console.log(`${error}`)}; // logs error if there is one
-          console.log(`Downloaded and saved ${data.length} bytes to ${args[1]}`);
-          process.exit();
-        });
-        
+        readFrom(localPath);
       } else {
         console.log(`${ans} - Stopping download... Exiting now.`);
         process.exit();
       }
-      
     });
-  } else if (!args[1]) {
-
-    fs.writeFile(args[1], body, error => {
-      if(error) { console.log( error )}; // logs error if there is one
-    });
-
-    fs.readFile(args[1], (error, data) => {
-      if(error) { console.log(`${error}`)}; // logs error if there is one
-      console.log(`Downloaded and saved ${data.length} bytes to ${args[1]}`);
-      process.exit();
-    });
+  } else if (!localPath) {
+    writeTo(localPath, body);
+    readFrom(localPath);
   }
 });
 
--- fsFuncs.js
+++ fsFuncs.js
@@ -7,3 +7,24 @@ const rl = readline.createInterface({
   input: stdin,
   output: stdout,
 });
+
+// Later use args[1] for path, and body for body.
+const writeTo = (path, body) => {
+  fs.writeFile(path, body, error => {
+    if(error) { console.log( error )}; // logs error if there is one
+  });
+};
+
+const readFrom = (path) => {
+  fs.readFile(path, (error, data) => {
+    if(error) { console.log(`${error}`)}; // logs error if there is one
+    console.log(`Downloaded and saved ${data.length} bytes to ${path}`);
+    process.exit();
+  });
+};
+
+module.exports = {
+rl,
+writeTo,
+readFrom,
+};
